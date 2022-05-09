4838450b9c2f4a7e3ac61e6c17ea87770984233b
underscoreDev <godswill@zimmerman.team>
2022-05-09 23:51:47 +0100

    feat: wrote first unit tests

--- src/first.ts
+++ src/first.ts
@@ -1,1 +1,16 @@
-export const myFunc = (num: number) => num * num;
+import arrays from "./utils/array";
+import numbers from "./utils/numbers";
+import strings from "./utils/strings";
+
+const numArr = [3, 4, 5, 6];
+const wordArr = ["cat", "dog", "rabbit", "bird"];
+const arrSum = arrays.addArr(numArr);
+const mixArr = arrays.concatArr(numArr, wordArr);
+const myNum = ("15" as unknown as number) % 2;
+const five = parseInt("5");
+
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize("the quick brown fox"));
+console.log(numbers.multiply(five, 8));
+console.log(arrays.lgNum(mixArr));
